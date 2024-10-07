ea1de02cc3087f205f23e2d5fbb0ae0b251f529d
yanerobot <screwyuguys@gmail.com>
2024-10-07 23:44:14 +0300

    Added .gitignore and project files

---
+++ Employee Management/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+class Employee
+{
+	string firstName, lastName, fatherName;
+	DateTime birthDate;
+	string address;
+	string department;
+	string about;
+
+	public Employee(string firstName = "", string lastName = "", string fatherName = "", DateTime birthDate = default, string address = "", string department = "", string about = "")
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.fatherName = fatherName;
+		this.birthDate = birthDate;
+		this.address = address;
+		this.department = department;
+		this.about = about;
+	}
+
+	public void PrintName() => Console.WriteLine(firstName);
+
+	public bool UpdateData(string fieldName, string value)
+	{
+		var fields = typeof(Employee).GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+		foreach (var field in fields)
+		{
+			if (field.Name == fieldName)
+			{
+				field.SetValue(this, value);
+				return true;
+			}
+		}
+
+		Console.WriteLine($"{fieldName} was not found");
+		return false;
+	}
+}
---
+++ Employee Management/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace EmployeeDatabase
+{
+	using System;
+	using System.Data;
+	using System.Windows.Forms;
+
+	namespace EmployeeDatabase
+	{
+		public partial class Form1 : Form
+		{
+			public Form1()
+			{
+				InitializeComponent();
+				LoadData();
+			}
+
+			private void LoadData()
+			{
+				// Создание DataTable для хранения данных сотрудников
+				DataTable employeesTable = new DataTable();
+				employeesTable.Columns.Add("Name", typeof(string));
+				employeesTable.Columns.Add("Birth Date", typeof(DateTime));
+				employeesTable.Columns.Add("Address", typeof(string));
+				employeesTable.Columns.Add("Department", typeof(string));
+				employeesTable.Columns.Add("About", typeof(string));
+
+				// Пример данных (добавьте своих сотрудников здесь)
+				employeesTable.Rows.Add("Иванов Иван", new DateTime(1985, 1, 1), "г. Москва, ул. Ленина, д. 1", "IT", "Программист");
+				employeesTable.Rows.Add("Петров Петр", new DateTime(1990, 5, 15), "г. Санкт-Петербург, ул. Пушкина, д. 2", "HR", "Менеджер по кадрам");
+
+				// Привязка DataTable к DataGridView
+				dataGridView1.DataSource = employeesTable;
+
+				// Настройка внешнего вида DataGridView
+				dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+				dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+			}
+
+			private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+			{
+				// Проверка нажатой ячейки
+				if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+				{
+					string currentValue = dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString();
+					EditCellForm editForm = new EditCellForm(currentValue);
+
+					// Отображение окна редактирования
+					if (editForm.ShowDialog() == DialogResult.OK)
+					{
+						// Сохранение нового значения в таблицу
+						dataGridView1[e.ColumnIndex, e.RowIndex].Value = editForm.NewValue;
+					}
+				}
+			}
+		}
+	}
+
+}
